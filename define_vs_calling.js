92a14595bc7b6429f564ba27dd8ad144d4e1c7c6
Lubabalo007 <lmgqatsa72@gmail.com>
2016-11-15 13:46:39 +0200

    registration check

--- define_vs_calling.js
+++ define_vs_calling.js
@@ -1,15 +1,8 @@
-//execute this using define_vs_calling.js
-
-var theFunction = function(){
-    console.log('this is the function executing');
+var yearsAgo = function(Month){// declare the function
+  var year 1 = new Date (year 1)// converting to date format
+  var year 2 = new Date (year 2)
+  var result = year1.getYear()-year2.getfullYear();//
+  console.log (result);// Printing and displaying
+  return result;//
 }
-
-//what does this print
-console.log(theFunction);
-
-console.log(typeof theFunction);
-
-//to call the function
-theFunction();
-
-//only now 'this is the function' is printed out to the console
+yearsAgo('1980-03-07,''2016-11-11');// calling the function
--- regCheck.js
+++ regCheck.js
@@ -1,6 +1,5 @@
 var regCheck= function (regNum,location){
 var results = regNum.startsWith(location)
-console.log (results)
 return results
 }
 regCheck ('GP 123','GP');
