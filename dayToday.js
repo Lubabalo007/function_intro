a9a698730d65556a8d71df6b849db1263512d13f
Lubabalo007 <lmgqatsa72@gmail.com>
2016-11-16 14:20:27 +0200

    Tested

--- dayToday.js
+++ dayToday.js
@@ -1,8 +1,17 @@
-function dayToday(){
-var today = new Date();
-var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-return weekDays[today.getDay()];
+const assert = require ('assert');
+var dayToday = function (day){
+  const date = new Date('2010-04-01');
+  var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
+  //return index between 0 and 6
+  console.log(date.getDay());
 
+  //look up the week day text string from the Array
+  console.log(weekDays[date.getDay()]);
+  if (day==weekDays[date.getDay()]) {
+    return true
+  }else {
+    return false
+  }
 }
-console.log(dayToday());
+assert.equal (dayToday('Friday'), true);
