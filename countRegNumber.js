420cc21303938ce492ad33881a2d10acdfa80716
Lubabalo007 <lmgqatsa72@gmail.com>
2016-11-16 15:39:58 +0200

    I changed it

--- countRegNumber.js
+++ countRegNumber.js
@@ -4,6 +4,6 @@ var countRegNumber = function(param){
 var cars = param.split(',');
 return cars.length;
 
-
-};
+}
 countRegNumber('CA 182736,CY 523519,CJ 812328')
+assert.equal (countRegNumber("param"),true);
