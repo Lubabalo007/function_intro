580d7cdc368e1d9f57e54f2ac49b5894b5f067da
Lubabalo007 <lmgqatsa72@gmail.com>
2016-11-16 14:55:11 +0200

    got it

--- regCheck.js
+++ regCheck.js
@@ -1,7 +1,7 @@
 
-const assert = require ("assert");
+const assert=require ('assert');
 var regCheck= function (regNum,location){
 var results = regNum.startsWith(location)
 return results
 }
-regCheck ('GP 123','GP');
+assert.equal(regCheck ('GP 123','GP'), true);
