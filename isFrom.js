ca4c81878fc8f808898c9ddef93e05d3da47de6e
Lubabalo007 <lmgqatsa72@gmail.com>
2016-11-16 14:53:03 +0200

    chsnges

--- isFrom.js
+++ isFrom.js
@@ -1,5 +1,8 @@
+
+const assert = require ('assert');
 var isFrom = function (regNum,location){
 var results = regNum.startsWith(location)
 return results
 }
 isFrom ('CY 123','CY');
+assert.equal (isFrom("CY 123"),false);
